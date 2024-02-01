7bf55b77b0ac6560b84d18170d4ad8ad9435f2c4
RobsonTrasel <robson.trasel@gmail.com>
2024-01-31 23:44:08 -0300

    fix: put the @jsonproperty on some properties

--- src/main/java/com/apibrasil/sdk/dto/login/LoginRes.java
+++ src/main/java/com/apibrasil/sdk/dto/login/LoginRes.java
@@ -2,6 +2,7 @@ package com.apibrasil.sdk.dto.login;
 
 import com.apibrasil.sdk.dto.base.authorization.Authorization;
 import com.apibrasil.sdk.dto.base.user.User;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import java.util.List;
 import java.util.Map;
@@ -10,8 +11,11 @@ import java.util.Map;
 public class LoginRes {
     private boolean error;
     private String message;
+    @JsonProperty("user")
     private User user;
+    @JsonProperty("invoices")
     private List<Map<String, String>> invoices;
+    @JsonProperty("authorization")
     private Authorization authorization;
 
 }
