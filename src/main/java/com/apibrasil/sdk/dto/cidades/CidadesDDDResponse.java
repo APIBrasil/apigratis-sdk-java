c7bbbb31a9408260c19db132c781b25a3052de65
RobsonTrasel <robson.trasel@gmail.com>
2024-01-31 22:32:34 -0300

    feat: create a cidades por DDD response

---
+++ src/main/java/com/apibrasil/sdk/dto/cidades/CidadesDDDResponse.java
@@ -0,0 +1,22 @@
+package com.apibrasil.sdk.dto.cidades;
+
+import com.apibrasil.sdk.dto.base.cidade.Cidade;
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+public class CidadesDDDResponse {
+    private boolean error;
+    private String message;
+    private ResponseData response;
+    private int api_limit;
+    private String api_limit_for;
+    private int api_limit_used;
+
+    @Data
+    public static class ResponseData {
+        private List<Cidade> cidades;
+    }
+}
